0900cde184429b410f919acd11615113c8075407
André Faria <andfar10@gmail.com>
2023-06-05 00:39:29 +0100

    Integrated API VideoJS to play videos

--- PHP/Projeto/src/content/media.php
+++ PHP/Projeto/src/content/media.php
@@ -15,14 +15,14 @@ $serverPort = 81;
 
 $UrL = "http://" . $serverName . ":" . $serverPort;
 $videoPath = GetVideoPath($idContent);
-$videoPath1 = $UrL . "/examples-smi/Projeto/media/" . $videoPath;
+$videoPath1 = $UrL . "/examples-smi/Projeto/media/" . rawurlencode($videoPath);
 ?>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Media</title>
+    <link href="https://vjs.zencdn.net/8.3.0/video-js.css" rel="stylesheet" />
+    <!-- City -->
+    <link href="https://unpkg.com/@videojs/themes@1/dist/city/index.css" rel="stylesheet">
     <?php include '../lib/dependenciesLinks.php' ?>
 </head>
 
@@ -31,12 +31,18 @@ $videoPath1 = $UrL . "/examples-smi/Projeto/media/" . $videoPath;
         <div class="row">
             <?php include '../sidebarmenus/sidebar.php' ?>
             <div id="accordion" class="col-md-10 bg-lightest-gray">
-                <video id="video-player" controls>
-                    <source src="<?php echo $videoPath1; ?>" type="video/mp4">
-                    Your browser does not support the video tag.
+                <video id="my-video" class="video-js vjs-theme-city" controls preload="auto" width="640" height="264" data-setup="{}">
+                    <source src="<?php echo $videoPath1 ?>" type="video/mp4" />
+                    <source src="<?php echo $videoPath1 ?>" type="video/webm" />
+
+                    <p class="vjs-no-js">
+                        To view this video please enable JavaScript, and consider upgrading to a
+                        web browser that
+                        <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                    </p>
                 </video>
             </div>
         </div>
     </div>
+    <script src="https://vjs.zencdn.net/8.3.0/video.min.js"></script>
 </body>
-
